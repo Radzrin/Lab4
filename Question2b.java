855a0cd4a7a6cb6df96b2f5d25e23c01a0ce7dde
Radzrin <60394053+Radzrin@users.noreply.github.com>
2020-04-04 19:52:53 -0400

    Update Question2b.java

--- Question2b.java
+++ Question2b.java
@@ -5,42 +5,49 @@ import java.util.*;
  public class Question2b{ 
     public static void main(String[] args) {
 		
-	System.out.println("please put in your email");
-	System.out.println("");
+		 System.out.println("please put in your email");
+		 System.out.println("");
 		
-	Scanner sc = new Scanner(System.in);
-	//variables
-	String coma = ".";
-	String at = "@";
-	String email;
-	email = sc.nextLine();
-	int i = email.length();
+		Scanner sc = new Scanner(System.in);
+		
+		String comma = ".";
+		String at = "@";
+	    String email;
+	    String begining = "^[a-zA-Z0-9_+&*-]+(?:\\."+ "[a-zA-Z0-9_+&*-]+)*@"; 
+	    email = sc.nextLine();
+	    int i = email.length();
 	    
 	    
-	 //check to see if the email is too long or too short
-	for (i =0; i <=254; i++) {
+	    //check to see if the email is too long or too short
+	    for (i =0; i <=254; i++) {
         if (email ==null){
-          System.out.println("email invalid");
-          System.out.println("");
+        System.out.println("email invalid");
+        System.out.println("");
         break;
-	} else if (i > 254){
-	    System.out.println("Email invalid: email is too long");
-	break;}
-	}
+	    } else if (i > 254){
+	        System.out.println("Email invalid: email is too long");
+	    break;}
+	    }
          
         //program checks if @ and . are present
         for (i =0; i <=254; i++) {
         if(email.charAt(i) == '@') { 
-          i++
-       break;}
+                i++;
+        }else{ 
+        System.out.println("the email is invalid missing an @");
+       }
        
         for (i =0; i <=254; i++) {
         if(email.charAt(i) == '.') { 
-          i++;
-       }
-                
-      }
-     } 
+                i++;
+            }else {
+            System.out.println("");    
+             System.out.println("email invalid comma missing");   
+            break;}
+        }
+         } 
             
-   } 
- } 
+        } 
+         
+    } 
+
