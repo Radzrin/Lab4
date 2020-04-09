68f11e849561b9b815e3c2ce9f7d9fab83efa9aa
Radzrin <60394053+Radzrin@users.noreply.github.com>
2020-04-09 13:53:08 -0400

    Add files via upload

---
+++ Egg.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+public class Main{
+	public static void main(String[] args) {
+	  
+
+	    try{
+	    Scanner sc = new Scanner(System.in);
+	    int Num; 
+
+	    System.out.println("Put in a number");
+	    System.out.println("");
+	    Num = sc.nextInt();
+	    System.out.println("Thank you for your cooperation");
+	    }catch (Exception e) {
+		System.out.println("put in a damn number");
+		//so try catch is like the end of the class
+	
+	    try{
+		System.out.println("");
+	    Scanner sc = new Scanner(System.in);	
+		int Num = sc.nextInt();
+		System.out.println("");
+		System.out.println("Thank you for your cooperation");
+	    }catch (Exception f) {
+		System.out.println("screw you");
+	    }
+	    } 
+	}        
+}	
+
+
+
+
+
+
+
